1f4492c97f81b93b534c36d62e78c4396f3f7cee
Jason Calalang <jcalalang@ph.ibm.com>
2021-01-14 01:32:42 +0800

    Initial commit

---
+++ src/app/shared/customer.service.ts
@@ -0,0 +1,35 @@
+import { Customer } from "./customer.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment.prod";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CustomerService {
+  customers: Customer[];
+
+  readonly APIUrl = environment.backendUri;
+
+  constructor(private http: HttpClient) {}
+
+  PostCustomer(cust: Customer) {
+    return this.http.post(this.APIUrl, cust);
+  }
+
+  GetCustomerList() {
+    return this.http.get(this.APIUrl);
+  }
+
+  GetCustomerById(id: string) {
+    return this.http.get(this.APIUrl + `/${id}`);
+  }
+
+  PutCustomer(cust: Customer) {
+    return this.http.put(this.APIUrl + `/${cust._id}`, cust);
+  }
+
+  DeleteCustomer(custid: string) {
+    return this.http.delete(this.APIUrl + `/${custid}`);
+  }
+}
